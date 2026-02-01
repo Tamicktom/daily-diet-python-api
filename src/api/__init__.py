e16e6a4736c9a898b9ae6446b5c2aee937e85d54
Tamicktom <rickpokemon38@gmail.com>
2026-02-01 17:51:42 -0300

    Add SECRET_KEY and SQLALCHEMY_DATABASE_URI configuration to the Flask app

--- src/api/__init__.py
+++ src/api/__init__.py
@@ -2,8 +2,17 @@ from flask import Flask
 
 app = Flask(__name__)
 
+app.config["SECRET_KEY"] = "your_secret_key"
+app.config["SQLALCHEMY_DATABASE_URI"] = (
+    "mysql+pymysql://mysql:mysql@localhost:3306/mysql"
+)
+
 
 @app.route("/hello", methods=["GET"])
 def hello():
     """Return a hello world message."""
     return {"message": "Hello, World!"}, 200
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
