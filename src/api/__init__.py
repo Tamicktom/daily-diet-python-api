2666f41abf83997910f7d8d2ac426c2f6a9364ed
Tamicktom <rickpokemon38@gmail.com>
2026-02-02 23:21:27 -0300

    Refactor Flask app configuration to use environment variables and initialize database; register meal blueprint

--- src/api/__init__.py
+++ src/api/__init__.py
@@ -1,11 +1,18 @@
+# Libraries imports
 from flask import Flask
+
+# Local imports
+from api.env import config
+from api.database import db
+from api.controllers.meal_controller import meal_blueprint
 
 app = Flask(__name__)
 
-app.config["SECRET_KEY"] = "your_secret_key"
-app.config["SQLALCHEMY_DATABASE_URI"] = (
-    "mysql+pymysql://mysql:mysql@localhost:3306/mysql"
-)
+app.config["SECRET_KEY"] = config.SECRET_KEY
+app.config["SQLALCHEMY_DATABASE_URI"] = config.DATABASE_URL
+
+db.init_app(app)
+app.register_blueprint(meal_blueprint)
 
 
 @app.route("/hello", methods=["GET"])
